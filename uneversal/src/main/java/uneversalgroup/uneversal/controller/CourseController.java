292651030b83629abb2577954275d82ae003a91c
Azizbek <132741338+Azizbek-dev-ful@users.noreply.github.com>
2023-10-12 21:41:41 +0500

    get one

--- uneversal/src/main/java/uneversalgroup/uneversal/controller/CourseController.java
+++ uneversal/src/main/java/uneversalgroup/uneversal/controller/CourseController.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import uneversalgroup.uneversal.entity.Course;
 import uneversalgroup.uneversal.impl.controller.CourseControllerImpl;
 import uneversalgroup.uneversal.payload.ApiResponse;
 import uneversalgroup.uneversal.payload.CourseDto;
+import uneversalgroup.uneversal.repository.CourseRepository;
 import uneversalgroup.uneversal.service.CourseService;
 
+import java.lang.module.ResolutionException;
 import java.util.List;
 
 @RestController
@@ -17,9 +20,17 @@ import java.util.List;
 @RequestMapping("/api/course")
 public class CourseController implements CourseControllerImpl {
     private final CourseService courseService;
+    private final CourseRepository courseRepository;
 
     @Override
     @GetMapping("/{id}")
+    public HttpEntity<?> getOneCourse(@PathVariable Integer id) {
+        Course course = courseRepository.findById(id).orElseThrow(() -> new ResolutionException("getCourseId"));
+        return ResponseEntity.ok(course);
+    }
+
+    @Override
+    @GetMapping("/list")
     public HttpEntity<?> getCourse() {
         List<CourseDto> course = courseService.getCourse();
         return ResponseEntity.ok(course);
--- uneversal/src/main/java/uneversalgroup/uneversal/impl/controller/CourseControllerImpl.java
+++ uneversal/src/main/java/uneversalgroup/uneversal/impl/controller/CourseControllerImpl.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpEntity;
 import uneversalgroup.uneversal.payload.CourseDto;
 
 public interface CourseControllerImpl {
+     HttpEntity<?>getOneCourse(Integer id);
      HttpEntity<?>getCourse();
      HttpEntity<?>addCourse(CourseDto courseDto);
      HttpEntity<?>editeCourse(Integer id,CourseDto courseDto);
