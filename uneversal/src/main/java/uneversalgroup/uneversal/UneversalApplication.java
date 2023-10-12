654ca755cbda2404bbc01a35b512019ab3ea4d39
shavkatnazarov <130224808+shavkatnazarov@users.noreply.github.com>
2023-10-12 14:16:18 +0500

    donyoraziz

---
+++ uneversal/src/main/java/uneversalgroup/uneversal/UneversalApplication.java
@@ -0,0 +1,13 @@
+package uneversalgroup.uneversal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UneversalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UneversalApplication.class, args);
+	}
+
+}
---
+++ uneversal/src/test/java/uneversalgroup/uneversal/UneversalApplicationTests.java
@@ -0,0 +1,13 @@
+package uneversalgroup.uneversal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UneversalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
