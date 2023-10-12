9466f4e065b8ef9d4ea464e79b4b411301491b1f
shavkatnazarov <130224808+shavkatnazarov@users.noreply.github.com>
2023-10-12 15:59:35 +0500

    aplikishn

--- universal admin-panel/src/App.jsx
+++ universal admin-panel/src/App.jsx
@@ -1,9 +1,14 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Menyu} from "./pages/Menyu.jsx";
 
 function App() {
     return (
         <BrowserRouter>
+        <Routes>
 
+                <Route path={"/"} element={<Menyu/>}/>
+
+        </Routes>
         </BrowserRouter>
     )
 }
