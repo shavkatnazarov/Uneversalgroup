52a421d2910a3c975731399b45ebc7d6a454116b
Azizbek <132741338+Azizbek-dev-ful@users.noreply.github.com>
2023-10-13 14:28:05 +0500

    tugirladim

--- universal admin-panel/src/utils/IsStatus.js
+++
@@ -1,3 +0,0 @@
-export const IS_STATUS=(status)=>{
-return status===200||status===201||status===204
-}
--- universal admin-panel/src/utils/Utils.js
+++ universal admin-panel/src/utils/Utils.js
@@ -1,4 +1,4 @@
-export const ifStatus = (status) => {
+export const IS_STATUS = (status) => {
     return status === 200 || status === 201 || status === 204;
 }
 
