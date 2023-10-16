31cacaf927133b514e06723d827c3b3d77131196
shavkatnazarov <130224808+shavkatnazarov@users.noreply.github.com>
2023-10-16 15:00:56 +0500

    cc

--- universal admin-panel/src/connection/service/AppService.js
+++ universal admin-panel/src/connection/service/AppService.js
@@ -1,6 +1,19 @@
 //start payment
 import {BASE_CONFIG} from "../BaseConfig.js";
 import {APP_API} from "../AppApi.js";
+
+export const GetOnePayment = async (id) => {
+    try {
+        const res = await BASE_CONFIG.doGetOne(APP_API.payment, id)
+        return res.data
+    } catch (err) {
+        console.log(err.message)
+    }
+}
+//end payment
+//start payment
+import {BASE_CONFIG} from "../BaseConfig.js";
+import {APP_API} from "../AppApi.js";
 import {toast} from "react-toastify";
 import {IS_STATUS} from "../../utils/IsStatus.js";
 
