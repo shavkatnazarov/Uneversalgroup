c83e194279c70daad40731b61d4e694527bca066
Doniyor <hoshimovdoniyor90@gmail.com>
2023-10-21 17:45:27 +0500

    ds

--- universal admin-panel/src/templates/Menu.jsx
+++ universal admin-panel/src/templates/Menu.jsx
@@ -2,6 +2,8 @@ import  {useEffect, useState} from "react";
 import {Card, CardBody, CardHeader} from "react-bootstrap";
 import {APP_API} from "../connection/AppApi.js";
 import {BASE_CONFIG as BaseConfig} from "../connection/BaseConfig.js";
+import {Course} from "../admin/course/Course.jsx";
+import {Link} from "react-router-dom";
 
 export const Menu = () => {
     const [loading, setLoading] = useState(false)
@@ -21,10 +23,10 @@ export const Menu = () => {
     }, [])
 
     const statArr = [
-        {name: "uqituvchi", statistika: stat.pupilSize, icon: 'fa-light fa-chalkboard-user', about: "uqituvchilar soni"},
-        {name: "uquvchilar", statistika: stat.teacherSize, icon: 'fas fa-users', about: 'uquvchilarsoni  soni'},
-        {name: "courslar", statistika: stat.courseSize, icon: 'fas fa-chart-pie', about: 'kurslar soni'},
-        {name: "guruhlar", statistika: stat.groupSize, icon: 'fas fa-percent', about: 'guruhlar soni'}
+        {name: "O'qituvchi", statistika: stat.pupilSize, icon: 'fas fa-users', about: "O'qituvchilar soni",link:'/auth/dashboard/teacher'},
+        {name: "O'quvchilar", statistika: stat.teacherSize, icon: 'fas fa-users', about: 'O\'quvchilarsoni  soni',link:'/auth/dashboard/pupil'},
+        {name: "Kursalar", statistika: stat.courseSize, icon: 'fas fa-chart-pie', about: 'Kurslar soni',link:'/auth/dashboard/course'},
+        {name: "Guruhlar", statistika: stat.groupSize, icon: 'fas fa-percent', about: 'Guruhlar soni',link:'/auth/dashboard/group'}
     ]
     return (
 
@@ -39,16 +41,18 @@ export const Menu = () => {
             <div className={"d-flex align-items-center justify-content-between"}>
             {statArr.map(item => (
                 <Card style={{width:"300px"}}>
-                    <CardHeader className={"d-flex align-items-center justify-content-between"}>
+                    <CardHeader className={"d-flex align-items-center justify-content-between"} >
                     <i className={item.icon}/>
                         <h5 className="card-title  text-uppercase text-muted mb-0">{item.name}</h5>
                     </CardHeader>
                     <CardBody>
-                       <div> <span
-                           className="h2 font-weight-bold mb-0">{item.statistika}
-                        </span>
+                       <div>
                        </div>
-                        <div> <span className="text-nowrap">{item.about}</span></div>
+                        <div> <span className="text-nowrap "  style={{fontSize:'25px'}}>{item.about}: <span
+                            className=" font-weight-bold mb-0">{item.statistika}
+                        </span></span> <br/>
+                        <Link to={item.link} className={"btn btn-success"}>Ko'rish</Link>
+                        </div>
                     </CardBody>
                 </Card>
             ))}
